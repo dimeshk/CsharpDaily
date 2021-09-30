f99725fe2dfeff41fa913eefa650be04b0a21031
Dimesh <dimeshdk@gmail.com>
2021-09-30 11:53:56 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Calculate obj = new Calculate();
+
+            int sum2 = obj.Add(1, 2);
+            Console.WriteLine("{0}", sum2);
+
+
+            int sum3 = obj.Add(1, 2, 3);
+            Console.WriteLine("{0}", sum3);
+        }
+    }
+}
+
