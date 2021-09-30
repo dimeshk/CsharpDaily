500a0fff3940188dc772e0a6405f23f73c682992
Dimesh <dimeshdk@gmail.com>
2021-09-30 11:58:30 +0530

    adding all files

---
+++ Calculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Calculate
+    {
+      public int Add(int a, int b)
+        {
+            int sum;
+            sum = a + b;
+
+            return sum;
+            
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            int sum;
+            sum = a + b + c;
+
+            return sum;
+        }
+
+
+    }
+}
